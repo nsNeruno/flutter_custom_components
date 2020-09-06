00515b9e0cfaf633223a1e8917504e043a8f8056
neruno <infinia249@gmail.com>
2020-09-06 23:30:00 +0700

    Updated main.dart to show sample page

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,58 +1,119 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_custom_components/flutter_custom_components.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(TestApp(),);
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterCustomComponents.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+class TestApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+      title: "Soluix Flutter Components",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TestPage(),
+    );
+  }}
+
+class TestPage extends StatefulWidget {
+  @override
+  _TestPageState createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Test App",)
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RaisedButton(
+            child: Text("Test Loading Alert Dialog",),
+            onPressed: _testLoadingAlertDialog,
+          ).maxWidth,
+          RaisedButton(
+            child: Text("Open PIN Page",),
+            onPressed: _openPinPage,
+          ).maxWidth,
+          RaisedButton(
+            child: Text("Test Date Picker",),
+            onPressed: _pickDate,
+          ),
+          RaisedButton(
+            child: Text("Test Time Picker",),
+            onPressed: _pickTime,
+          ),
+        ],
+      ).paddedAll(24.0,),
+    );
+  }
+
+  void _testLoadingAlertDialog() {
+    LoadingAlertDialog.showLoadingAlertDialog<void>(
+      context: context,
+      builder: (_) => AlertDialog(
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              color: Colors.white,
+              child: SizedBox.fromSize(
+                size: Size.square(24.0,),
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          ],
         ),
       ),
+      computation: Future.delayed( Duration(seconds: 3,), ),
     );
+  }
+
+  void _openPinPage() {
+    Navigator.of(context,).push(
+      MaterialPageRoute(
+        builder: (context) {
+//          Future.delayed(Duration(seconds: 5,), () {
+//            Navigator.of(context).push(
+//              MaterialPageRoute(builder: (_) => TestPage(),),
+//            );
+//          },);
+          return PinEntryPage(
+            appBar: AppBar(),
+            useDeviceKeyboard: true,
+          );
+        },
+      ),
+    );
+  }
+
+  void _pickDate() {
+    final now = DateTime.now();
+    showPlatformDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: now.subtract(Duration(days: 30,),),
+      lastDate: now.add(Duration(days: 30,),),
+    ).then((date) {
+      debugPrint(date.toString(),);
+    },);
+  }
+
+  void _pickTime() {
+    final now = DateTime.now();
+    showPlatformTimePicker(
+      context: context,
+      initialDate: now,
+    ).then((time) {
+      debugPrint(time.toString(),);
+    });
   }
 }
